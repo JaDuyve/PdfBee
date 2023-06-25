c8ec7be2933dfca50f8e77ef8598d581fe7df35f
Jari Duyvejonck <jari.duyvejonck@outlook.com>
2023-06-25 21:38:16 +0200

    init project

---
+++ app.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+// Greet returns a greeting for the given name
+func (a *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) OpenFileDialog() string {
+	options := runtime.OpenDialogOptions{
+		Filters: []runtime.FileFilter{
+			{
+				DisplayName: "pdf",
+				Pattern:     "*.pdf",
+			},
+		},
+	}
+	file, err := runtime.OpenFileDialog(a.ctx, options)
+	if err != nil {
+		runtime.LogError(a.ctx, err.Error())
+	}
+
+	runtime.LogInfo(a.ctx, file)
+	return file
+}
